112fbc4a000604efa132f329efda51b6a358fc7a
triangletristan <123992885+triangletristan@users.noreply.github.com>
2024-07-02 22:09:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,22 @@ let button5 = document.getElementById("button5");
 let K = document.getElementById("K");
 let button6 = document.getElementById("button6");
 let SP = document.getElementById("SP");
+let button7 = document.getElementById("button7");
+let L = document.getElementById("L");
+let button8 = document.getElementById("button8");
+let AH = document.getElementById("AH");
+let button9 = document.getElementById("button9");
+let APCC = document.getElementById("APCC");
 var clickbutton1 = false
 var clickbutton2 = false
 var clickbutton3 = false
 var clickbutton4 = false
 var clickbutton5 = false
 var clickbutton6 = false
+var clickbutton7 = false
+var clickbutton8 = false
+var clickbutton9 = false
+
 
 function IsClicked(number){
   if (number == 1){
@@ -82,6 +92,39 @@ function IsClicked(number){
       button6.innerText = 'Spongebob Train Project (2023)   < ';
       clickbutton6 = false;
       SP.style.display ="none";
+    }
+  }
+  if (number == 7){
+    if (clickbutton7 === false){
+      button7.innerText = 'Lightsaber Project (2024)   v ';
+      clickbutton7 = true;
+      L.style.display ="block";
+    } else {
+      button7.innerText = 'Lightsaber Project (2024)   < ';
+      clickbutton7 = false;
+      L.style.display ="none";
+    }
+  }
+  if (number == 8){
+    if (clickbutton8 === false){
+      button8.innerText = 'Air-Hockey Machine Project (2024)   v ';
+      clickbutton8 = true;
+      AH.style.display ="block";
+    } else {
+      button8.innerText = 'Air-Hockey Machine Project (2024)   < ';
+      clickbutton8 = false;
+      AH.style.display ="none";
+    }
+  }
+  if (number == 9){
+    if (clickbutton9 === false){
+      button9.innerText = 'AP Calculus Capstone Project (2024)   v ';
+      clickbutton9 = true;
+      APCC.style.display ="block";
+    } else {
+      button9.innerText = 'AP Calculus Capstone Project (2024)   < ';
+      clickbutton9 = false;
+      APCC.style.display ="none";
     }
   }
 }
